8ac9f46e75df264f3dee21fe0fa6c2f750de1b86
David Lee <davidomundo@gmail.com>
2019-08-29 14:01:24 -0700

    Fix

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-alias apt='UCF_FORCE_CONFOLD=1 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qq -y'
+UCF_FORCE_CONFOLD=1 DEBIAN_FRONTEND=noninteractive
+alias apt=apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qq -y
 apt update
 apt upgrade
 apt install software-properties-common
