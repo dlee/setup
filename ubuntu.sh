3562a0c296cafe5c1c8d3fd5dd6d1580be603862
David Lee <davidomundo@gmail.com>
2019-08-29 13:59:17 -0700

    force non-interactive apt

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
+alias apt=UCF_FORCE_CONFOLD=1 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -qq -y
 apt update
-apt upgrade -y
-apt install -y software-properties-common
+apt upgrade
+apt install software-properties-common
 add-apt-repository -y ppa:keithw/mosh
 add-apt-repository -y ppa:neovim-ppa/stable
 add-apt-repository -y -r ppa:chris-lea/node.js
 apt update
-apt install -y curl git build-essential nodejs fasd neovim mosh zsh tmux
+apt install curl git build-essential nodejs fasd neovim mosh zsh tmux
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
 dpkg -i ripgrep_11.0.2_amd64.deb
 echo "Changing shell to ZSH"
@@ -14,10 +15,11 @@ chsh -s /usr/bin/zsh
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --bin
 [ ! -f ~/.ssh/id_rsa.pub ] && cat /dev/zero | ssh-keygen -q -N ""
-read -p "Enter Github OTP: " OTP
+unset GITHUB_OTP
+read -p "Enter Github OTP: " GITHUB_OTP
 echo "Uploading Github SSH Key"
 curl -u "dlee" \
-    --header "x-github-otp: $OTP" \
+    --header "x-github-otp: $GITHUB_OTP" \
     --data "{\"title\":\"`hostname`_`date +%Y%m%d%H%M%S`\",\"key\":\"`cat ~/.ssh/id_rsa.pub`\"}" \
     https://api.github.com/user/keys
 cd ~
