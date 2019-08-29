e2f26c1b72c97e50d0f58393894a253544e70768
David Lee <davidomundo@gmail.com>
2019-08-29 13:19:55 -0700

    Install tmux

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,7 +6,7 @@ add-apt-repository ppa:keithw/mosh
 add-apt-repository ppa:neovim-ppa/stable
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt update
-apt install -y git build-essential nodejs fasd neovim mosh zsh
+apt install -y git build-essential nodejs fasd neovim mosh zsh tmux
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
 dpkg -i ripgrep_11.0.2_amd64.deb
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
